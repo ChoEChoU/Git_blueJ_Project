f910c1ce8495d72be267f719ce58d7d27a77e356
Lim Mintaek <lmtna99@gmail.com>
2019-09-02 14:00:41 +0900

    sd

--- Circle.java
+++ Circle.java
@@ -1,17 +1,18 @@
-
-/**
- * 여기에 Circle 클래스 설명을 작성하십시오.
- * 
- * @author (작성자 이름) 
- * @version (버전번호나 날짜)
- */
 public class Circle implements Shape
 {
-    double Pi = 3.14;
-    void draw();
-    double getArea();
-    default public void redraw(){
-        System.out.print("---다시 그립니다. ");
-        draw();
+    private int radius;
+
+    public Circle(int radius)
+    {
+        this.radius = radius;
+    }
+    
+    public double getArea()
+    {
+        return radius*radius*PI;
+    }
+    
+    public void draw(){
+        System.out.print("반지름이 " + this.radius + "인 원입니다.");
     }
 }
